ff3b9d92ee07c6120ca43fee8fd8c1c105ceb9a9
Rally Jinx <rsjinx@gmail.com>
2017-01-09 10:35:41 -0600

    little things to try to fix the crashing

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-if (process.env.NODE_ENV != 'production') {
+//if (process.env.NODE_ENV != 'production') {
   require('dotenv').config();
-}
+//}
 
 const express = require('express');
 const app = express();
